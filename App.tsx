4a244dbe33fa88c0017161a8a646bedda3358eeb
chris.lee@myhomevault.com.au <chris.lee@myhomevault.com.au>
2022-04-06 17:27:03 +1000

    Google login

--- App.tsx
+++ App.tsx
@@ -1,61 +1,57 @@
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import {Button} from 'react-native';
-import {StyleSheet, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+import * as Notification from 'expo-notifications';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-	// const [request, response, promptAsync] = Google.useAuthRequest({
-	// 	expoClientId: '737634999521-t5ugefdpqa5m6r2g698iel229895q76o.apps.googleusercontent.com',
-	// 	iosClientId: '737634999521-t5ugefdpqa5m6r2g698iel229895q76o.apps.googleusercontent.com',
-	// 	webClientId: '737634999521-t5ugefdpqa5m6r2g698iel229895q76o.apps.googleusercontent.com',
-	// 	scopes: ['profile', 'email'],
-	// 	redirectUri: 'http://localhost:8000/oauth/google/redirect',
-	// 	responseType: 'code',
-	// 	extraParams: {
-	// 		access_type: 'offline',
-	// 		prompt: 'consent',
-	// 	}
-	// });
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		expoClientId: '737634999521-t5ugefdpqa5m6r2g698iel229895q76o.apps.googleusercontent.com',
+		iosClientId: '737634999521-t5ugefdpqa5m6r2g698iel229895q76o.apps.googleusercontent.com',
+		webClientId: '737634999521-t5ugefdpqa5m6r2g698iel229895q76o.apps.googleusercontent.com',
+		scopes: ['profile', 'email', 'youtube', 'youtube.upload'],
+		responseType: 'token',
+		extraParams: {
+			prompt: 'consent',
+		}
+	});
 
-	// React.useEffect(() => {
-	// 	if (response?.type === 'success') {
-	// 		const { authentication } = response;
-	// 		console.log('authentication', authentication)
-	// 	}
-	// }, [response]);
+	const handlePressAsync = async () => {
+		const result = await promptAsync();
+		if (result.type !== "success") {
+			return;
+		}
+		const {access_token} = result.params;
+		// Generate push notification token then send together with access_token
+		// const expoPushToken = await Notification.getExpoPushTokenAsync()
 
-	// const handlePressAsync = async () => {
-	// 	const result = await promptAsync();
-	// 	console.log(result)
-	// 	if (result.type !== "success") {
-	// 		return;
-	// 	}
-	// 	const {access_token} = result.params;
-	// 	console.log('access_token=', access_token)
-	// 	const userInfoResponse = await fetch(
-	// 		`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`
-	// 	);
-	// 	const userInfo = await userInfoResponse.json();
-	// 	console.log('userInfo=', userInfo)
-	// };
+		// Simulator is not supported
+		const expoPushToken = 'ExponentPushToken[BrFf4kBEsRZTzDYsU3QviW]'
 
-	const requestGoogleAuth = async () => {
-		const getAuthUrl = await fetch(
-			`http://localhost:8000/oauth/google`
-		);
-		const data = await getAuthUrl.json();
-		console.log(data)
-		await WebBrowser.openBrowserAsync(data.url);
-	}
+		const response = await fetch('http://localhost:8000/auth/oauth', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				oAuthType: 'GOOGLE',
+				accessToken: access_token,
+				expoPushToken: expoPushToken,
+			})
+		})
+		const responseResult = await response.json();
+		console.log(responseResult)
+	};
 
 	return (
 		<View style={styles.container}>
 			<Button
 				title="Google Login"
-				onPress={requestGoogleAuth}
+				onPress={async () => {
+					await handlePressAsync();
+				}}
 			/>
 		</View>
 	);
